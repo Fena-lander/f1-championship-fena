7ad10d1beb93f4030aa03f135761c96aeb3e0a98
Felipe van Oorshot <66143166+Fena-lander@users.noreply.github.com>
2023-12-16 02:04:43 -0300

    Add pilots in championship

--- F1-Championship/Form6.cs
+++ F1-Championship/Form6.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,8 @@ namespace F1_Championship
     {
         private List<Championship> championshipNameSelect;
 
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Championship.json");
+
         public Form6(List<Championship> championshipNameSelect)
         {
             this.championshipNameSelect = championshipNameSelect;
@@ -22,7 +26,6 @@ namespace F1_Championship
             InitializeComponent();
             dataGridView1.DataSource = null;
             dataGridView1.DataSource = allPilots;
-
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -42,12 +45,61 @@ namespace F1_Championship
 
         private void carNumber_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+            carNumber.MaxLength = 10;
         }
 
         private void addPilot_Click(object sender, EventArgs e)
         {
+            if (carNumber.Text == "")
+            {
+                MessageBox.Show("Número do carro deve ser preenchido");
+                return;
+            }
 
+            string name = pilotName.Text;
+            int pilotCarNumber = int.Parse(carNumber.Text);
+            int points = 0;
+
+            List<Pilot> allPilots = championshipNameSelect.SelectMany(championship => championship.Pilots).ToList();
+            Pilot newPilot = new Pilot { Name = name, CarNumber = pilotCarNumber, Points = points };
+
+            if (name == "")
+            {
+                MessageBox.Show("Nome do piloto deve ser preenchido");
+                return;
+            }
+            else
+            {
+                foreach (var pilot in allPilots)
+                {
+                    if (pilot.CarNumber == pilotCarNumber)
+                    {
+                        MessageBox.Show("Esse número de carro já existe");
+                        carNumber.Text = string.Empty;
+                        return;
+                    }
+                }
+
+                allPilots.Add(newPilot);
+            }
+
+            foreach (Championship championship in championshipNameSelect)
+            {
+                championship.Pilots = allPilots;
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = allPilots;
+
+            string updatedJson = JsonConvert.SerializeObject(championshipNameSelect, Formatting.Indented);
+            File.WriteAllText(filePath, updatedJson);
+
+            pilotName.Text = string.Empty;
+            carNumber.Text = string.Empty;
         }
 
         private void finishChampionship_Click(object sender, EventArgs e)
@@ -56,6 +108,12 @@ namespace F1_Championship
             Form5 newChampionship = new Form5(championshipNameSelect);
             newChampionship.FormClosed += (s, args) => this.Close();
             newChampionship.Show();
+        }
+
+        private void clearPilotsParameter_Click(object sender, EventArgs e)
+        {
+            pilotName.Text = string.Empty;
+            carNumber.Text = string.Empty;
         }
     }
 }
